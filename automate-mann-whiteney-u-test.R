fedd3deb3d4b76b110381c4ec707ef0430e96812
milankarunarathne <mhkarunarathne@gmail.com>
2018-01-24 12:14:06 +0530

    Adding GC calls for reduce terminate rsession

--- automate-mann-whiteney-u-test.R
+++ automate-mann-whiteney-u-test.R
@@ -1,27 +1,30 @@
-data = c("/home/milan/twice_max/1/twice_max_1/twice_max_1.csv", "/home/milan/twice_max/2/twice_max_2/twice_max_2.csv")
-result = data
+data = c("/home/milan/twice_max/1/twice_max_1/twice_max_1.csv", "/home/milan/twice_max/2/twice_max_2/twice_max_2.csv", "/home/milan/twice_max/3/twice_max_3/twice_max_3.csv", "/home/milan/twice_max/4/twice_max_4/twice_max_4.csv", "/home/milan/twice_max/5/twice_max_5/twice_max_5.csv", "/home/milan/twice_max/6/twice_max_6/twice_max_6.csv", "/home/milan/twice_max/7/twice_max_7/twice_max_7.csv", "/home/milan/twice_max/8/twice_max_8/twice_max_8.csv", "/home/milan/twice_max/9/twice_max_9/twice_max_9.csv", "/home/milan/twice_max/10/twice_max_10/twice_max_10.csv")
 for(i in 1:length(data)){
   print(data[i])
+  print("read Master csv")
   dataM <- read.csv(data[i],header = T) # read csv of the before scenario
   dataM <- data.frame(dataM)
+  print("master csv reading completed")
+  gc()
   keeps <- c("Latency")
   dataM <- dataM[, keeps, drop=TRUE] # only select values of the "Latency" column
-  dataM
+  gc()
   for(j in 1:length(data)){
-    print(data[j])
+    print("read Current csv")
     dataC <- read.csv(data[j],header = T) # read csv of the before scenario
     dataC <- data.frame(dataC)
+    print("current csv reading completed")
+    gc()
     dataC <- dataC[, keeps, drop=TRUE] # only select values of the "Latency" column
-    dataC
+    gc()
+    print("please wait, wilcox test")
     result <- wilcox.test(dataM,dataC, correct = FALSE)
     print(result)
     print("remove Current object and gc")
     rm(dataC)
-    gc(TRUE) 
+    gc()
   }
   print("remove Master object and gc")
   rm(dataM)
-  gc(TRUE)
-} 
-write.csv(result, file = "results.csv")
-
+  gc()
+}
