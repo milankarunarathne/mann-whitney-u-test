003dc06c0094d2dc8e85150fb14f88b2b38528b8
milankarunarathne <mhkarunarathne@gmail.com>
2018-01-25 15:12:58 +0530

    Adding summary csv creator and Improve performance

--- automate-mann-whiteney-u-test.R
+++ automate-mann-whiteney-u-test.R
@@ -1,4 +1,10 @@
-data = c("/home/milan/twice_max/1/twice_max_1/twice_max_1.csv", "/home/milan/twice_max/2/twice_max_2/twice_max_2.csv", "/home/milan/twice_max/3/twice_max_3/twice_max_3.csv", "/home/milan/twice_max/4/twice_max_4/twice_max_4.csv", "/home/milan/twice_max/5/twice_max_5/twice_max_5.csv", "/home/milan/twice_max/6/twice_max_6/twice_max_6.csv", "/home/milan/twice_max/7/twice_max_7/twice_max_7.csv", "/home/milan/twice_max/8/twice_max_8/twice_max_8.csv", "/home/milan/twice_max/9/twice_max_9/twice_max_9.csv", "/home/milan/twice_max/10/twice_max_10/twice_max_10.csv")
+data = c("/home/milan/twice_min/1/jtls/twice_min_1.csv", "/home/milan/twice_min/6/jtls/twice_min_6.csv")
+# data = c("/home/milan/twice_max/1/twice_max_1/twice_max_1.csv", "/home/milan/twice_max/6/twice_max_6/twice_max_6.csv")
+summary_csv = "summary.csv" # summary csv location
+result_current = cbind("First CSV","Second CSV","X","Y","W Value","P Value")
+print(result_current)
+write.table(result_current, file = summary_csv, col.names = FALSE, row.names = FALSE, sep = ",")
+# write.csv(data, file = summary_csv, sep = ",", append = TRUE, quote = TRUE, eol = "\n")
 for(i in 1:length(data)){
   print(data[i])
   print("read Master csv")
@@ -10,16 +16,38 @@ for(i in 1:length(data)){
   dataM <- dataM[, keeps, drop=TRUE] # only select values of the "Latency" column
   gc()
   for(j in 1:length(data)){
-    print("read Current csv")
-    dataC <- read.csv(data[j],header = T) # read csv of the before scenario
-    dataC <- data.frame(dataC)
-    print("current csv reading completed")
-    gc()
-    dataC <- dataC[, keeps, drop=TRUE] # only select values of the "Latency" column
-    gc()
-    print("please wait, wilcox test")
-    result <- wilcox.test(dataM,dataC, correct = FALSE)
-    print(result)
+    if(i==j){
+      print(data[j])
+      print("read Current csv")
+      print("Master csv = Current csv")
+      dataC <- dataM
+      print("please wait, wilcox test")
+      result <- wilcox.test(dataM,dataC, correct = FALSE)
+      # write.csv(i, file = summary_csv, append = TRUE, sep = ",")
+      print(result)
+      result_current <- cbind(data[i], data[j],i,j,result$statistic,result$p.value)
+      print(result_current)
+      write.table(result_current, file = summary_csv, col.names = FALSE, row.names = FALSE, sep = ",", append = TRUE)
+      # print(result)
+    }else{
+      print(data[j])
+      print("read Current csv")
+      dataC <- read.csv(data[j],header = T) # read csv of the before scenario
+      dataC <- data.frame(dataC)
+      print("current csv reading completed")
+      gc()
+      dataC <- dataC[, keeps, drop=TRUE] # only select values of the "Latency" column
+      gc()
+      print("please wait, wilcox test")
+      result <- wilcox.test(dataM,dataC, correct = FALSE)
+      print(result)
+      result_current <- cbind(data[i], data[j],i,j,result$statistic,result$p.value)
+      print(result_current)
+      write.table(result_current, file = summary_csv, col.names = FALSE, row.names = FALSE, sep = ",", append = TRUE)
+      # write.csv(i, file = summary_csv, append = TRUE, sep = ",", eol = "")
+      # write.csv(result, file = summary_csv, append = TRUE, sep = ",", eol = "\n")
+      # print(result)
+    }
     print("remove Current object and gc")
     rm(dataC)
     gc()
